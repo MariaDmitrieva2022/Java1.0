966bca32041240e01df4bba1a5cb29ae0e5f8794
mashalifeworld@gmail.com <mashalifeworld@gmail.com>
2022-04-03 12:49:10 +0300

    Правки

--- src/com/company/lesson/Animal2.java
+++ src/com/company/lesson/Animal2.java
@@ -3,5 +3,6 @@ package com.company.lesson;
 public class Animal2 {
     public static void main(String[] args) {
         System.out.println("lesson6");
+        System.out.println("lesson6");
     }
 }
