c3fdc6658606cc3d26a66d944dd27a4207a66529
mashalifeworld@gmail.com <mashalifeworld@gmail.com>
2022-04-06 21:22:34 +0300

    lesson7

---
+++ src/com/company/lesson6/Cat.java
@@ -0,0 +1,11 @@
+package com.company.lesson6;
+
+public class Cat extends Animal {
+    public Cat() {
+        maxRunDistance = 200;
+        name = "Кот";
+    }
+    public void swim(int distanceSwim) {
+            System.out.println(name + " не умеет плавать.");
+    }
+}
